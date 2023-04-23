8f66fde4773eca7c51247f8a0a7714b601919936
WilliamGLYork <wdgl500@york.ac.uk>
2023-04-23 11:15:39 +0100

    Fixed "RunWith" for textures. Added Cooked and Baked Status Test

--- tests/src/de/catepilla/gdxtesting/examples/IngredientTests.java
+++ tests/src/de/catepilla/gdxtesting/examples/IngredientTests.java
@@ -2,20 +2,60 @@ package de.catepilla.gdxtesting.examples;
 
 import Ingredients.Ingredient;
 import Ingredients.Onion;
+import de.catepilla.gdxtesting.GdxTestRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.assertEquals;
 
-public class IngredientTests {
+/** NB:
+ * Private method findCorrectSkin() is for implementation only and does not need to be tested.
+ */
 
+@RunWith (GdxTestRunner.class)
+public class IngredientTests {
     @Test
     public void IngredientPreparedStatus(){
-        Ingredient ingredient = new Onion(1,0);
+        Ingredient ingredient = new Onion(1,0, 0);
 
         assertEquals(false, ingredient.isPrepared());
 
         ingredient.setPrepared();
 
-        assertEquals(false, ingredient.isPrepared(), );
+        assertEquals(true, ingredient.isPrepared());
 
     }
+
+    @Test
+    public void IngredientCookedStatus(){
+        Ingredient ingredient = new Onion(1,0, 0);
+
+        assertEquals(false, ingredient.isCooked());
+
+        ingredient.setCooked();
+
+        assertEquals(true, ingredient.isCooked());
+
+    }
+
+    @Test
+    public void IngredientBakedStatus(){
+        Ingredient ingredient = new Onion(1,0,0);
+
+        assertEquals(false, ingredient.isBaked());
+
+        ingredient.setBaked();
+
+        assertEquals(true, ingredient.isBaked());
+
+    }
+
+    public void IngredientCreateTexture(){
+
+    }
+
+    public void IngredientCreatePosAndSize(){
+
+    }
+
 }
