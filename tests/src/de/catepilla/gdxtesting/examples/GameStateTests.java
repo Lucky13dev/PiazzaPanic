65552024bf5873350f2ab9ffc460b168a86d362c
WilliamGLYork <wdgl500@york.ac.uk>
2023-04-27 13:26:32 +0100

    Added InteractiveTileObjectTests and GameStateTests

---
+++ tests/src/de/catepilla/gdxtesting/examples/GameStateTests.java
@@ -0,0 +1,53 @@
+package de.catepilla.gdxtesting.examples;
+
+
+import Sprites.Chef;
+import Tools.B2WorldCreator;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.team13.piazzapanic.GameState;
+import com.team13.piazzapanic.MainGame;
+import com.team13.piazzapanic.PlayScreen;
+import de.catepilla.gdxtesting.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(GdxTestRunner.class)
+public class GameStateTests {
+
+    @Test
+    public void TestMaxChefs() {
+        GameState gameState = new GameState();
+        int max_count = GameState.getMAX_CHEF_COUNT();
+        int expected_max_count = 5;
+        assertEquals(expected_max_count, max_count);
+    }
+
+    @Test
+    public void TextChefIndex() {
+        World world;
+        TiledMap map;
+        PlayScreen screen;
+        MainGame maingame = new MainGame();
+        screen = new PlayScreen(maingame);
+        world = new World(new Vector2(0,0), true);
+        TmxMapLoader mapLoader = new TmxMapLoader(new InternalFileHandleResolver());
+        map = mapLoader.load("Kitchen.tmx");
+        new B2WorldCreator(world, map, screen);
+
+        GameState gameState = new GameState();
+        gameState.addChef(new Chef(world, 31.5F,65));
+        gameState.setControlledChef(1);
+        Chef controlledChef = gameState.getControlledChef();
+        assertEquals(1, controlledChef);
+
+    }
+
+}
