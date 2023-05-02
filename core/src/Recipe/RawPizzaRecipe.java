65c8508e1d51c986217d8e38d3376c3a01666c8e
Benji-Young <benjaminty2003@gmail.com>
2023-05-02 19:58:04 +0100

    Updated comments in Recipe package

--- core/src/Recipe/RawPizzaRecipe.java
+++ core/src/Recipe/RawPizzaRecipe.java
@@ -7,6 +7,13 @@ import com.badlogic.gdx.graphics.Texture;
 
 import java.util.ArrayList;
 
+/**
+
+ The RawPizzaRecipe class is a subclass of the Recipe class and represents a raw pizza in the kitchen game.
+ It holds an ArrayList of ingredients needed to make a raw pizza and a Texture of the completed dish image.
+ The ingredients in the ArrayList consist of a {@link Ingredients.PizzaBase} and a {@link Ingredients.Cheese}
+ and a {@link Ingredients.Tomato}.
+ */
 public class RawPizzaRecipe extends Recipe{
 
     public RawPizzaRecipe(){
